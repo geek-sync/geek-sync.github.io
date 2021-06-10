1a125084fef6141811bd02d310cc2fedeabf7029
Guruprasadbhat <54360365+GURUPRAD1999@users.noreply.github.com>
2021-06-10 20:59:13 +0530

    first

---
+++ firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+    apiKey: "AIzaSyCKtjy5KRMIXAS7nwgaIM8Ttr4HywiLaeA",
+    authDomain: "webx-d813f.firebaseapp.com",
+    projectId: "webx-d813f",
+    storageBucket: "webx-d813f.appspot.com",
+    messagingSenderId: "359536501733",
+    appId: "1:359536501733:web:d4d93b1f8bffba2a14050c",
+    measurementId: "G-B7W6103Z16"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
