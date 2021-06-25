629b7e7fa43735bab4ee091beada405b566ee3f3
geek-sync <64354709+geek-sync@users.noreply.github.com>
2021-06-25 20:24:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     var firebaseConfig = {
-    apiKey: "AIzaSyCKtjy5KRMIXAS7nwgaIM8Ttr4HywiLaeA",
-    authDomain: "webx-d813f.firebaseapp.com",
-    projectId: "webx-d813f",
-    storageBucket: "webx-d813f.appspot.com",
-    messagingSenderId: "359536501733",
-    appId: "1:359536501733:web:d4d93b1f8bffba2a14050c",
-    measurementId: "G-B7W6103Z16"
+    apiKey: "AIzaSyBvH4dYBpOxGPuTrqayCfvHcy6GXKroVsk",
+    authDomain: "dustbinbot.firebaseapp.com",
+    databaseURL: "https://dustbinbot-default-rtdb.firebaseio.com",
+    projectId: "dustbinbot",
+    storageBucket: "dustbinbot.appspot.com",
+    messagingSenderId: "550209807171",
+    appId: "1:550209807171:web:25adcea9437e7e401785f6",
+    measurementId: "G-YP03XR9LTY"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
