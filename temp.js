59f69ca04beb38ad1d9228af3a68c45c3ed3d758
geek-sync <64354709+geek-sync@users.noreply.github.com>
2021-06-25 20:24:03 +0530

    Delete temp.js

--- temp.js
+++
@@ -1,9 +0,0 @@
- var rootRef = firebase.database();
-     
-    //creates ref for node
-   var ref=rootRef.ref('Data/')
-   ref.on('value',function(snapshot){
-   var data= snapshot.val()
-    document.getElementById("answer").innerHTML += data.temp + "&degC";
-        
-   });
